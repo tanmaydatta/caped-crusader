0e946bd73626efc9306e32f786b592b2ddad3a3a
Tanmay Datta <tanmaydatta@gmail.com>
2014-12-01 21:54:20 +0530

    added migrations

--- caped_crusader/caped_crusader/migrations/0022_auto__add_contactus__del_field_codeforces_id__chg_field_codeforces_han.py
+++ caped_crusader/caped_crusader/migrations/0022_auto__add_contactus__del_field_codeforces_id__chg_field_codeforces_han.py
@@ -8,40 +8,45 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Deleting field 'user.spoj'
-        db.delete_column(u'caped_crusader_user', 'spoj')
+        # Adding model 'contactUs'
+        db.create_table(u'caped_crusader_contactus', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('Name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('Phone', self.gf('django.db.models.fields.CharField')(max_length=100, null=True)),
+            ('Email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('Message', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal(u'caped_crusader', ['contactUs'])
 
-        # Deleting field 'user.codechef'
-        db.delete_column(u'caped_crusader_user', 'codechef')
+        # Deleting field 'Codeforces.id'
+        db.delete_column(u'caped_crusader_codeforces', u'id')
 
-        # Deleting field 'user.codeforces'
-        db.delete_column(u'caped_crusader_user', 'codeforces')
 
-        # Deleting field 'user.topcoder'
-        db.delete_column(u'caped_crusader_user', 'topcoder')
+        # Changing field 'Codeforces.handle'
+        db.alter_column(u'caped_crusader_codeforces', 'handle', self.gf('django.db.models.fields.CharField')(max_length=100, primary_key=True))
+        # Adding unique constraint on 'Codeforces', fields ['handle']
+        db.create_unique(u'caped_crusader_codeforces', ['handle'])
 
 
     def backwards(self, orm):
-        # Adding field 'user.spoj'
-        db.add_column(u'caped_crusader_user', 'spoj',
-                      self.gf('django.db.models.fields.CharField')(max_length=100, null=True),
+        # Removing unique constraint on 'Codeforces', fields ['handle']
+        db.delete_unique(u'caped_crusader_codeforces', ['handle'])
+
+        # Deleting model 'contactUs'
+        db.delete_table(u'caped_crusader_contactus')
+
+
+        # User chose to not deal with backwards NULL issues for 'Codeforces.id'
+        raise RuntimeError("Cannot reverse this migration. 'Codeforces.id' and its values cannot be restored.")
+        
+        # The following code is provided here to aid in writing a correct migration        # Adding field 'Codeforces.id'
+        db.add_column(u'caped_crusader_codeforces', u'id',
+                      self.gf('django.db.models.fields.AutoField')(primary_key=True),
                       keep_default=False)
 
-        # Adding field 'user.codechef'
-        db.add_column(u'caped_crusader_user', 'codechef',
-                      self.gf('django.db.models.fields.CharField')(max_length=100, null=True),
-                      keep_default=False)
 
-        # Adding field 'user.codeforces'
-        db.add_column(u'caped_crusader_user', 'codeforces',
-                      self.gf('django.db.models.fields.CharField')(max_length=100, null=True),
-                      keep_default=False)
-
-        # Adding field 'user.topcoder'
-        db.add_column(u'caped_crusader_user', 'topcoder',
-                      self.gf('django.db.models.fields.CharField')(max_length=100, null=True),
-                      keep_default=False)
-
+        # Changing field 'Codeforces.handle'
+        db.alter_column(u'caped_crusader_codeforces', 'handle', self.gf('django.db.models.fields.CharField')(max_length=100))
 
     models = {
         u'caped_crusader.codechef': {
@@ -66,8 +71,7 @@ class Migration(SchemaMigration):
         u'caped_crusader.codeforces': {
             'Meta': {'object_name': 'Codeforces'},
             'college': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['caped_crusader.College']", 'null': 'True'}),
-            'handle': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'handle': ('django.db.models.fields.CharField', [], {'max_length': '100', 'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True'}),
             'orank': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
             'orating': ('django.db.models.fields.IntegerField', [], {'default': '-1'}),
@@ -77,6 +81,14 @@ class Migration(SchemaMigration):
         u'caped_crusader.college': {
             'Meta': {'object_name': 'College'},
             'collegeName': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'caped_crusader.contactus': {
+            'Email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'Message': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'Meta': {'object_name': 'contactUs'},
+            'Name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'Phone': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
         },
         u'caped_crusader.topcoder': {
@@ -90,12 +102,12 @@ class Migration(SchemaMigration):
             'rating': ('django.db.models.fields.IntegerField', [], {'default': '-1'})
         },
         u'caped_crusader.user': {
-            'Email': ('django.db.models.fields.EmailField', [], {'default': "'test@test.com'", 'max_length': '75'}),
+            'Email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
             'Firstname': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'Lastname': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True'}),
             'Meta': {'object_name': 'user'},
             'Password': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'college': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': u"orm['caped_crusader.College']"}),
+            'Username': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
         }
     }
